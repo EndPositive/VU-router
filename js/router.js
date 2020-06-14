f1a42e5f9956197a1987eb197cf1406a6ec1f26b
Edward2k <edulira@me.com>
2020-06-14 18:13:41 +0200

    Added first few routes

--- js/router.js
+++ js/router.js
@@ -7,6 +7,8 @@ function populate_routes() {
     next_button.disabled = current_route === max_routes - 1;
     prev_button.disabled = current_route === 0;
 
+    const path_to_route_image = random_path();
+
     let route_text = document.getElementById("route_text");
     route_text.textContent = "Route " + (current_route + 1);
 
@@ -20,7 +22,7 @@ function populate_routes() {
     heat.textContent = (current_route * 22 + 21) + " nearby";
 
     let route = document.getElementById("route");
-    route.innerText = "PLACEHOLDER ROUTE " + (current_route + 1);
+    route.innerHTML = '<img src="' +  path_to_route_image + '" >';
 
 }
 
@@ -35,4 +37,26 @@ function prev_route() {
         current_route--;
         populate_routes();
     }
+}
+
+function random_path() {
+    const path = Math.floor(Math.random() * 100) % 4
+    var path_to_route_image = "../routes/";
+    switch (path) {
+        case 0: 
+            path_to_route_image += "route1.png"
+            break;
+        case 1:
+            path_to_route_image += "route2.png"
+            break;
+        case 2:
+            path_to_route_image += "route2.png"
+            break;
+        default:
+            path_to_route_image += "route2.png"
+            break;
+    }
+    console.log(path)
+    console.log(path_to_route_image);
+    return path_to_route_image;
 }
