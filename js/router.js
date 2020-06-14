3f8e85a05180421f989078dfdd37eaac043e8419
Jop Zitman <jop-zitman@hotmail.com>
2020-06-14 17:03:13 +0200

    Add choose route page (router.html)

---
+++ js/router.js
@@ -0,0 +1,38 @@
+let current_route = 0;
+let max_routes = 3;
+
+function populate_routes() {
+    let next_button = document.getElementById("next_button");
+    let prev_button = document.getElementById("prev_button");
+    next_button.disabled = current_route === max_routes - 1;
+    prev_button.disabled = current_route === 0;
+
+    let route_text = document.getElementById("route_text");
+    route_text.textContent = "Route " + (current_route + 1);
+
+    let ETA = document.getElementById("ETA");
+    ETA.textContent = "ETA: " + (current_route * current_route + 3) + "min";
+
+    let steps = document.getElementById("steps");
+    steps.textContent = (current_route * current_route * 10 + 54) + " steps";
+
+    let heat = document.getElementById("heat");
+    heat.textContent = (current_route * 22 + 21) + " nearby";
+
+    let route = document.getElementById("route");
+    route.innerText = "PLACEHOLDER ROUTE " + (current_route + 1);
+
+}
+
+function next_route() {
+    if (current_route < max_routes - 1) {
+        current_route++;
+        populate_routes();
+    }
+}
+function prev_route() {
+    if (current_route > 0) {
+        current_route--;
+        populate_routes();
+    }
+}
